16a6f816200125a26e0691e3d36df9054d9fc412
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-04-25 19:08:58 +0300

    Update SRP

---
+++ SingleResponsabilityPrinciple/SRPBadExample/SRPBadExample/BadExample.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using algo_data_structures;
+
+namespace SRP
+{
+    class Equations
+    {
+        public double GetRootLinearEquation(linearEquation Equation)
+        {
+            double Root = 0;
+
+            if (Equation.slope.Equals(0))
+                throw new System.ArgumentException("Slope can not be zero");
+            if (Equation.y_intercept.Equals(0))
+                throw new System.ArgumentException("y intercept can not be zero");
+
+            Root = -Equation.y_intercept / Equation.slope;
+
+            return Root;
+        }
+
+        public double GetValueQuadraticEquation(quadraticEquation Equation, limits<double> Roots)
+        {
+
+            double Result;
+
+            Result = ((Equation.coefficient_a * Math.Pow(Roots.minimLimit, 2) + (Equation.coefficient_b * Roots.maximLimit) + (Equation.coefficient_c)));
+
+            return Result;
+        }
+    }
+
+    class BadExample
+    {
+        static void Main(string[] args)
+        {
+            linearEquation Linear = new linearEquation();
+            quadraticEquation Quadratic = new quadraticEquation();
+            Equations EquationsMethods = new Equations();
+            limits<double> Roots = new limits<double>();
+
+            Linear.slope = 5;
+            Linear.y_intercept = 7;
+
+            Console.WriteLine(EquationsMethods.GetRootLinearEquation(Linear));
+
+            Quadratic.coefficient_a = 1;
+            Quadratic.coefficient_b = 4;
+            Quadratic.coefficient_c = 3;
+
+            Roots.minimLimit = -3;
+            Roots.maximLimit = -1;
+
+            Console.WriteLine(EquationsMethods.GetValueQuadraticEquation(Quadratic, Roots));
+        }
+    }
+}
---
+++ SingleResponsabilityPrinciple/SRPGoodExample/SRPGoodExample/GoodExample.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using algo_data_structures;
+
+namespace SRP
+{
+    class Validations
+    {
+        public void CheckIfZero<Type> (Type Value)
+        {
+            if (Value.Equals(0))
+                throw new System.ArgumentException("Cannot work with parameters assigned to 0");
+        }
+    }
+
+    class LinearEquationOperations
+    {
+        public double GetRootLinearEquation(Validations validations, linearEquation Equation)
+        {
+            validations.CheckIfZero<double>(Equation.slope);
+            validations.CheckIfZero<double>(Equation.y_intercept);
+
+            double Root = 0;
+
+            Root = -Equation.y_intercept / Equation.slope;
+
+            return Root;
+        }
+    }
+    class QuadraticEquationOperations
+    {
+        public double GetValueQuadraticEquation(quadraticEquation Equation, limits<double> Roots)
+        {
+            double Result;
+
+            Result = ((Equation.coefficient_a * Math.Pow(Roots.minimLimit, 2) + (Equation.coefficient_b * Roots.maximLimit) + (Equation.coefficient_c)));
+
+            return Result;
+        }
+    }
+ 
+
+    class BadExample
+    {
+        static void Main(string[] args)
+        {
+            linearEquation Linear = new linearEquation();
+            quadraticEquation Quadratic = new quadraticEquation();
+
+            Validations validations = new Validations();
+
+            LinearEquationOperations LinearOperations = new LinearEquationOperations();
+            QuadraticEquationOperations QuadraticOperations = new QuadraticEquationOperations();
+
+            limits<double> Roots = new limits<double>();
+
+            Linear.slope = 5;
+            Linear.y_intercept = 7;
+
+            Console.WriteLine(LinearOperations.GetRootLinearEquation(validations, Linear));
+
+            Quadratic.coefficient_a = 1;
+            Quadratic.coefficient_b = 4;
+            Quadratic.coefficient_c = 3;
+
+            Roots.minimLimit = -3;
+            Roots.maximLimit = -1;
+
+            Console.WriteLine(QuadraticOperations.GetValueQuadraticEquation(Quadratic, Roots));
+        }
+    }
+}
