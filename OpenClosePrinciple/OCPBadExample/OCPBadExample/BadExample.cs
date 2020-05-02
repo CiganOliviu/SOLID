ddd02e9ee2881762e4f41c50d24983dad0c61fdb
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-05-02 18:25:40 +0300

    Update OCP

--- OpenClosePrinciple/OCPBadExample/OCPBadExample/BadExample.cs
+++ OpenClosePrinciple/OCPBadExample/OCPBadExample/BadExample.cs
@@ -1,12 +1,56 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace OCPBadExample
 {
+    class Circle
+    {   
+        public static double GetArea(Double Radius)
+        {
+            const double PI = 3.1415926535897931;
+            double result;
+
+            result = 2 * PI * Radius;
+
+            return result;
+        }
+    }
+
+    class Triangle
+    {
+
+        public static double GetArea(Double Base, Double Height)
+        {
+            double result;
+
+            result = Base * Height / 2;
+
+            return result;
+        }
+    }
+
+    class ShapesProcessor
+    {
+        public static void OutputAllAreasShapes(List<String> Shapes, int SizeOfList)
+        {
+            for (int i = 0; i < SizeOfList; i++)
+            {
+                if (Shapes[i] == "circle")
+                    Console.WriteLine(Circle.GetArea(3.0));
+                if (Shapes[i] == "triangle")
+                    Console.WriteLine(Triangle.GetArea(3.0, 4.0));
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            List<String> Shapes = new List<String>();
+            Shapes.Add("circle");
+            Shapes.Add("triangle");
 
+            ShapesProcessor.OutputAllAreasShapes(Shapes, 2);
         }
     }
 }
--- OpenClosePrinciple/OCPGoodExample/OCPGoodExample/GoodExample.cs
+++ OpenClosePrinciple/OCPGoodExample/OCPGoodExample/GoodExample.cs
@@ -2,11 +2,73 @@
 
 namespace OCPGoodExample
 {
+    interface Shapes
+    {
+        double GetArea();
+    }
+
+    class Circle: Shapes
+    {
+
+        private const double PI = 3.1415926535897931;
+        public double Radius;
+
+        public Circle (double Radius)
+        {
+            this.Radius = Radius;
+        }
+        public double GetArea()
+        {
+            double Result;
+
+            Result = 2 * PI * Radius;
+
+            return Result;
+        }
+    }
+
+    class Triangle: Shapes
+    {
+        double Base;
+        double Height;
+        
+        public Triangle (double Base, double Height)
+        {
+            this.Base = Base;
+            this.Height = Height;
+        } 
+        public double GetArea ()
+        {
+            double Result;
+
+            Result = Base * Height / 2;
+
+            return Result;
+        }
+    }
+
+    class ShapesProcessor
+    {
+        public static void OutputAllAreasShapes (Shapes[] ShapeInstances, int SizeOfArray)
+        {
+            for (int i = 0; i < SizeOfArray; i++)
+            {
+                Console.WriteLine(ShapeInstances[i].GetArea());
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Shapes[] ShapeInstance = new Shapes[2];
+            
+            ShapeInstance[0] = new Circle(3.0);
+            ShapeInstance[1] = new Triangle(3.0, 4.0);
+            
+            int Size = 2;
 
+            ShapesProcessor.OutputAllAreasShapes(ShapeInstance, Size);
         }
     }
 }
