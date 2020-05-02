56eef1402feba18c06b23313dc7f85519c49fa79
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-05-02 21:37:18 +0300

    Update ISP

--- InterfaceSegregationPrinciple/ISPBadExample/ISPBadExample/ISPBadExample.cs
+++ InterfaceSegregationPrinciple/ISPBadExample/ISPBadExample/ISPBadExample.cs
@@ -2,11 +2,72 @@
 
 namespace ISPBadExample
 {
+
+    interface ShapesAttributes
+    {
+        double GetArea();
+        double GetVolume();
+    }
+
+    class Cube : ShapesAttributes
+    {
+
+        private double Width;
+        public void SetWidth (double Width)
+        {
+            this.Width = Width;
+        }
+        public double GetArea()
+        {
+            double Result;
+
+            Result = 6 * Math.Pow(Width, 2);
+
+            return Result;
+        }
+        public double GetVolume()
+        {
+            double Result;
+
+            Result = Math.Pow(Width, 3);
+
+            return Result;
+        }
+    }
+
+    class Triangle : ShapesAttributes
+    {
+        private double Base;
+        private double Height;
+        
+        public void SetTriangleAttributes (double Base, double Height)
+        {
+            this.Base = Base;
+            this.Height = Height;
+        }
+        public double GetArea()
+        {
+            double Result;
+
+            Result = (Base * Height) / 2;
+
+            return Result;
+        }
+
+        public double GetVolume()
+        {
+            double Result = 0.0;
+
+            return Result;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Triangle triangle = new Triangle();
 
+            triangle.GetVolume();
         }
     }
 }
--- InterfaceSegregationPrinciple/ISPGoodExample/ISPGoodExample/ISPGoodExample.cs
+++ InterfaceSegregationPrinciple/ISPGoodExample/ISPGoodExample/ISPGoodExample.cs
@@ -2,11 +2,60 @@
 
 namespace ISPGoodExample
 {
+    interface ShapeAttributes
+    {
+        double GetArea();
+    }
+    interface SolidShapesAttribute
+    {
+        double GetVolume();
+    }
+
+    class Cube : SolidShapesAttribute
+    {
+        private double Width;
+        public void SetWidth(double Width)
+        {
+            this.Width = Width;
+        }
+        public double GetVolume()
+        {
+            double Result;
+
+            Result = Math.Pow(Width, 3);
+
+            return Result;
+        }
+    }
+
+    class Triangle : ShapeAttributes
+    {
+        private double Base;
+        private double Height;
+
+        public void SetTriangleAttributes(double Base, double Height)
+        {
+            this.Base = Base;
+            this.Height = Height;
+        }
+        public double GetArea()
+        {
+            double Result;
+
+            Result = (Base * Height) / 2;
+
+            return Result;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Cube cube = new Cube();
+            Triangle triangle = new Triangle();
+
+            cube.GetVolume();
+            triangle.GetArea();
         }
     }
 }
