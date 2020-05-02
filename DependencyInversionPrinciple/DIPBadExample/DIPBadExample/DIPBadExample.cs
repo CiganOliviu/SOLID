4048a27018a3df3d706ea4554389a681cbef0728
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-05-02 22:16:20 +0300

    Update DIP

--- DependencyInversionPrinciple/DIPBadExample/DIPBadExample/DIPBadExample.cs
+++ DependencyInversionPrinciple/DIPBadExample/DIPBadExample/DIPBadExample.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using algo_data_structures;
 
 namespace DIPBadExample
 {
+    class LinearEquationMath
+    {
+        public float GetValueLinearEquation ()
+        {
+            linearEquation Equation = new linearEquation();
+
+            float Result;
+
+            Result = (Equation.slope * Equation.root) + Equation.y_intercept;
+
+            return Result;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            LinearEquationMath LEMath = new LinearEquationMath();
 
+            LEMath.GetValueLinearEquation();
         }
     }
 }
--- DependencyInversionPrinciple/DIPGoodExample/DIPGoodExample/DIPGoodExample.cs
+++ DependencyInversionPrinciple/DIPGoodExample/DIPGoodExample/DIPGoodExample.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using algo_data_structures;
 
 namespace DIPGoodExample
 {
+
+    class LinearEquationMath
+    {
+        public float GetValueLinearEquation (linearEquation Equation)
+        {
+            float Result;
+
+            Result = (Equation.slope * Equation.root) + Equation.y_intercept;
+
+            return Result;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            LinearEquationMath LEMath = new LinearEquationMath();
+            linearEquation Equation = new linearEquation();
 
+            LEMath.GetValueLinearEquation(Equation);
         }
     }
 }
