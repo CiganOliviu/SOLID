e8bfcd162ed3622fa29a440bd125134fa4e0b45d
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-05-02 19:13:07 +0300

    Update Liskov

--- LiskovPrinciple/LPBadExample/LPBadExample/LPBadExample.cs
+++ LiskovPrinciple/LPBadExample/LPBadExample/LPBadExample.cs
@@ -2,11 +2,36 @@
 
 namespace LPBadExample
 {
+
+    class Rectangle
+    {
+        public double Height;
+        public double Width;
+
+        public void AssignAttributes(double Height, double Width)
+        {
+            this.Height = Height;
+            this.Width = Width; 
+        }
+    }
+
+    class Square : Rectangle
+    {
+        public void AssignAttributese(double Height, double Width)
+        {
+            this.Height = Height;
+            this.Width = Height;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Rectangle rectangle = new Rectangle();
+            Square square = new Square();
+
+            rectangle.AssignAttributes(3, 4);
+            square.AssignAttributes(3, 3);
         }
     }
 }
--- LiskovPrinciple/LPGoodExample/LPGoodExample/LPGoodExample.cs
+++ LiskovPrinciple/LPGoodExample/LPGoodExample/LPGoodExample.cs
@@ -2,11 +2,35 @@
 
 namespace LPGoodExample
 {
+    class Rectangle
+    {
+        double Height;
+        double Width;
+        public void AssignAttributes(double Height, double Width)
+        {
+            this.Height = Height;
+            this.Width = Width;
+        }
+    }
+
+    class Square
+    {
+        double Height;
+
+        public void AssignAttributes(double Height)
+        {
+            this.Height = Height;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Rectangle rectangle = new Rectangle();
+            Square square = new Square();
 
+            rectangle.AssignAttributes(3, 4);
+            square.AssignAttributes(3);
         }
     }
 }
